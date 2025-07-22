498e282fc821c64a91337ffd7ddbee13fcff225c
Diego Zapata <99824449+Zilith@users.noreply.github.com>
2025-07-21 23:30:46 -0500

    Add author field to BlogForm component
    The BlogForm now includes an input for the blog author, with corresponding state and change handler props. This change improves blog creation by allowing users to specify the author directly in the form.

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,49 +1,45 @@
-import blogService from '../services/blogs';
-
 const BlogForm = ({
+  handleNewBlog,
   newBlogTitle,
   setNewBlogTitle,
   newBlogUrl,
   setNewBlogUrl,
-}) => {
-  const handleNewBlog = async () => {
-    try {
-      const newBlog = {
-        title: newBlogTitle,
-        url: newBlogUrl,
-      };
-      const response = await blogService.create(newBlog);
-      return response;
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  return (
-    <>
-      <h2>Add New Blog</h2>
-      <form onSubmit={handleNewBlog}>
-        <div>
-          title
-          <input
-            type="text"
-            value={newBlogTitle}
-            name="newBlogTitle"
-            onChange={(target) => setNewBlogTitle(target.value)}
-          />
-        </div>
-        <div>
-          url
-          <input
-            type="text"
-            value={newBlogUrl}
-            name="newBlogUrl"
-            onChange={(target) => setNewBlogUrl(target.value)}
-          />
-        </div>
-        <button type="submit">Add Blog</button>
-      </form>
-    </>
-  );
-};
+  newBlogAuthor,
+  setNewBlogAuthor,
+}) => (
+  <>
+    <h2>Add New Blog</h2>
+    <form onSubmit={handleNewBlog}>
+      <div>
+        title
+        <input
+          type="text"
+          value={newBlogTitle}
+          name="newBlogTitle"
+          onChange={({ target }) => setNewBlogTitle(target.value)}
+        />
+      </div>
+      <div>
+        url
+        <input
+          type="text"
+          value={newBlogUrl}
+          name="newBlogUrl"
+          onChange={({ target }) => setNewBlogUrl(target.value)}
+        />
+      </div>
+      <div>
+        author
+        <input
+          type="text"
+          value={newBlogAuthor}
+          name="newBlogAuthor"
+          onChange={({ target }) => setNewBlogAuthor(target.value)}
+        />
+      </div>
+      <button type="submit">Add Blog</button>
+    </form>
+  </>
+);
 
 export default BlogForm;
